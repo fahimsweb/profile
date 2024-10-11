2588cf8581b9eccfaf1f1890ab92925f869d8504
Fahim Mohammed <fahimoha@cisco.com>
2024-10-10 20:18:16 -0500

    (base): Initial app scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      {/* Delete this before you start}*/}
+      <div className="flex min-h-screen items-center justify-center font-mono">
+        <h1 className="text-7xl font-bold">Base Profile Template</h1>
+      </div>
+      {/* Delete this before you start}*/}
+    </>
+  );
+}
+
+export default App;
